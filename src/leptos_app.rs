8c60c6e8d06b6994c1a62619997b3ee6c8a598b4
derui <derutakayu@gmail.com>
2025-09-13 17:09:25 +0900

    feat: add initial app

---
+++ src/leptos_app.rs
@@ -0,0 +1,30 @@
+use bevy::color::{palettes::tailwind::*, Srgba};
+use leptos::prelude::*;
+use leptos_bevy_canvas::prelude::*;
+
+#[component]
+pub fn App() -> impl IntoView {
+    view! {
+        <div class="flex gap-5 items-center p-5 mx-auto w-full max-w-[1400px]">
+            <Frame
+                class="border-red-500 flex-4 bg-red-500/5"
+                {..}
+            >
+                <div class="float-right">Click on a cube to select</div>
+
+                <h2 class="relative text-xl font-bold text-red-500 top-[-10px]">Bevy</h2>
+                <div
+                    class="overflow-hidden rounded-lg aspect-[8/5]"
+                    style:max-width="100%"
+                    style:max-height="100%"
+                >
+                </div>
+            </Frame>
+        </div>
+    }
+}
+
+#[component]
+pub fn Frame(class: &'static str, children: Children) -> impl IntoView {
+    view! { <div class=format!("border-2 border-solid {class} rounded-lg p-5")>{children()}</div> }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod leptos_app;
+
+use leptos::{
+    leptos_dom::logging::{console_debug_error, console_error},
+    mount::{self, mount_to, mount_to_body},
+};
+
 fn main() {
-    println!("Hello, world!");
+    console_error_panic_hook::set_once();
+
+    mount_to_body(leptos_app::App)
 }
