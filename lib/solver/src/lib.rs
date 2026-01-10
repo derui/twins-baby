bb1432577059ec1d08a43d22cf47f969985c26ce
derui <derutakayu@gmail.com>
2026-01-10 09:37:16 +0900

    feat: add a new member of solver

--- lib/solver/src/lib.rs
+++ lib/solver/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::{environment::Environment, matrix::sparse::SparseMatrix};
 
 pub mod environment;
@@ -9,6 +11,23 @@ pub mod vector;
 /// Internal Jacobian matrix. It is a matrix of constraint matrix.
 struct Jacobian(SparseMatrix<Box<dyn equation::Equation>>);
 
+/// Wrapper of Equation Id
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct EquationId(u64);
+
+/// Convertion logics
+impl From<u64> for EquationId {
+    fn from(value: u64) -> Self {
+        EquationId(value)
+    }
+}
+
+impl From<EquationId> for u64 {
+    fn from(value: EquationId) -> Self {
+        value.0
+    }
+}
+
 /// Solver struct.
 pub struct Solver {
     /// Current jacobian. It is updated when equation or variable updated.
@@ -19,4 +38,8 @@ pub struct Solver {
 
     /// dimensions for solver. There are similar to constant while solving
     dimensions: Environment,
+
+    /// current equations with equation id. Currently, equation id is not
+    /// member of equation mod, because equation does not have identity of it.
+    equations: HashMap<EquationId, Box<dyn equation::Equation>>,
 }
