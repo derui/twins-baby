0dde25c38593eda17860c5b01ec08d301fec36d9
derui <derutakayu@gmail.com>
2026-01-31 15:22:21 +0900

    feat(cad-base): immutable Constraint

--- lib/cad-base/src/sketch/constraint.rs
+++ lib/cad-base/src/sketch/constraint.rs
@@ -1,13 +1,26 @@
+use anyhow::Result;
+use immutable::Im;
 use solver::equation::Equation;
 
-use crate::id::VariableId;
+use crate::{
+    id::{ConstraintId, VariableId},
+    sketch::scope::VariableScope,
+};
 
 /// Constraint between variables
 #[derive(Debug, Clone)]
 pub struct Constraint {
+    pub id: Im<ConstraintId>,
+
     /// A equation
-    equation: Equation,
+    pub equation: Im<Equation>,
 
     /// A variables related of equation
-    related_variables: Vec<VariableId>,
+    pub related_variables: Im<Vec<VariableId>>,
+}
+
+impl Constraint {
+    pub fn new(_id: ConstraintId, _equation: Equation, _scope: &VariableScope) -> Result<Self> {
+        todo!()
+    }
 }
