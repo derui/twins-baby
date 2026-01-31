7054ae444e09775badc411499004d82cbbb4f909
derui <derutakayu@gmail.com>
2026-01-31 15:47:44 +0900

    feat(cad-base): add constructor

--- lib/cad-base/src/sketch/constraint.rs
+++ lib/cad-base/src/sketch/constraint.rs
@@ -1,6 +1,8 @@
+use std::collections::HashSet;
+
 use anyhow::Result;
 use immutable::Im;
-use solver::equation::Equation;
+use solver::equation::{Equation, Evaluate};
 
 use crate::{
     id::{ConstraintId, VariableId},
@@ -20,7 +22,43 @@ pub struct Constraint {
 }
 
 impl Constraint {
-    pub fn new(_id: ConstraintId, _equation: Equation, _scope: &VariableScope) -> Result<Self> {
-        todo!()
+    /// Make a new constraint with the equation.
+    ///
+    /// # Summary
+    /// This function will extract the related variables from the equation and map them to their corresponding VariableId using the provided VariableScope.
+    ///
+    /// # Arguments
+    /// * `id` - The unique identifier for the constraint.
+    /// * `equation` - The equation representing the constraint.
+    /// * `scope` - The variable scope used to map variable names to their IDs.
+    ///
+    /// # Returns
+    /// * `Result<Constraint>` - Returns a Result containing the newly created Constraint or an error if any variable in the equation is not found in the scope.
+    pub fn new(id: ConstraintId, equation: Equation, scope: &VariableScope) -> Result<Self> {
+        let mut vars: HashSet<String> = HashSet::from_iter(equation.related_variables());
+
+        let env = scope.to_id_name_map();
+        let mut related = vec![];
+
+        for var in vars.clone() {
+            if let Some(v) = env.get(&var) {
+                related.push(*v);
+                vars.remove(&var);
+            }
+        }
+
+        if related.len() != vars.len() {
+            let vars = vars.iter().cloned().collect::<Vec<_>>();
+            return Err(anyhow::anyhow!(
+                "Do not found variables {}",
+                vars.join(", ")
+            ));
+        }
+
+        Ok(Constraint {
+            id: id.into(),
+            equation: equation.into(),
+            related_variables: related.into(),
+        })
     }
 }
