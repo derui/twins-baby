6a777c8572c8b8af6ea19a7ce299ce265cd9593c
derui <derutakayu@gmail.com>
2026-02-07 09:39:44 +0900

    fix(app): suppress resize while hot-spot

--- src/leptos_app/resize_nob.rs
+++ src/leptos_app/resize_nob.rs
@@ -12,12 +12,9 @@ pub const NOB_AREA: u32 = 16;
 pub fn ResizeXNob(movement: WriteSignal<i32>, #[prop(optional)] class: String) -> impl IntoView {
     let (enable_move, set_enable_move) = signal(false);
 
-    let class = move || {
-        format!(
-            "absolute transparent w-[{}px] -translate-x-1/2 h-full {}",
-            NOB_AREA, class
-        )
-    };
+    let class = move || format!("absolute transparent -translate-x-1/2 h-full {}", class);
+
+    let style = move || format!("width: {}px", NOB_AREA);
 
     let mouse_down = move |_: MouseEvent| {
         set_enable_move.set(true);
@@ -33,13 +30,22 @@ pub fn ResizeXNob(movement: WriteSignal<i32>, #[prop(optional)] class: String) -
             return;
         }
 
+        if (ev.offset_y().abs() as u32) < NOB_AREA / 2 {
+            return;
+        }
+
         let moved = ev.movement_x();
         movement.set(moved);
     };
 
     view! {
-        <div class=class on:mousemove=mouse_move on:mousedown=mouse_down on:mouseup=mouse_up>
-        </div>
+        <div
+            class=class
+            style=style
+            on:mousemove=mouse_move
+            on:mousedown=mouse_down
+            on:mouseup=mouse_up
+        ></div>
     }
 }
 
@@ -52,12 +58,9 @@ pub fn ResizeXNob(movement: WriteSignal<i32>, #[prop(optional)] class: String) -
 pub fn ResizeYNob(movement: WriteSignal<i32>, #[prop(optional)] class: String) -> impl IntoView {
     let (enable_move, set_enable_move) = signal(false);
 
-    let class = move || {
-        format!(
-            "absolute transparent h-[{}px] -translate-y-1/2 w-full {}",
-            NOB_AREA, class
-        )
-    };
+    let class = move || format!("absolute transparent -translate-y-1/2 w-full {}", class);
+
+    let style = move || format!("height: {}px", NOB_AREA);
 
     let mouse_down = move |_: MouseEvent| {
         set_enable_move.set(true);
@@ -73,12 +76,21 @@ pub fn ResizeYNob(movement: WriteSignal<i32>, #[prop(optional)] class: String) -
             return;
         }
 
+        if (ev.offset_x().abs() as u32) < NOB_AREA / 2 {
+            return;
+        }
+
         let moved = ev.movement_y();
         movement.set(moved);
     };
 
     view! {
-        <div class=class on:mousemove=mouse_move on:mousedown=mouse_down on:mouseup=mouse_up>
-        </div>
+        <div
+            class=class
+            style=style
+            on:mousemove=mouse_move
+            on:mousedown=mouse_down
+            on:mouseup=mouse_up
+        ></div>
     }
 }
