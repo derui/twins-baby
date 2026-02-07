244b5cb5ed3037e57031af0863e078f9ecc51263
derui <derutakayu@gmail.com>
2026-02-07 16:38:37 +0900

    fix(cad_base_macro): macro expansion issue

--- lib/cad_base_macro/src/lib.rs
+++ lib/cad_base_macro/src/lib.rs
@@ -2,10 +2,9 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn;
 
 /// Derive [Id] to make cad-base's Id implementation
-#[proc_macro_derive(Id)]
+#[proc_macro_derive(MakeId)]
 pub fn id_derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
 
@@ -14,12 +13,14 @@ pub fn id_derive(input: TokenStream) -> TokenStream {
 
 fn impl_id_macro(ast: &syn::DeriveInput) -> TokenStream {
     let name = &ast.ident;
-    let type_name = ast.ident.to_string();
+    let mut type_name = ast.ident.to_string();
 
     if !type_name.ends_with("Id") {
         panic!("[Id] macro needs the type name ends with `Id`");
     }
-    
+    // trim name
+    type_name = type_name[0..(type_name.len() - 2)].to_string();
+
     let generated = quote! {
         impl #name {
             /// Creates a new [#name].
@@ -27,15 +28,22 @@ fn impl_id_macro(ast: &syn::DeriveInput) -> TokenStream {
                 #name(id)
             }
         }
+
         impl From<u64> for #name {
             fn from(id: u64) -> Self {
                 #name(id)
             }
         }
-        
-        impl Display for PlaneId {
+
+        impl From<#name> for u64 {
+            fn from(value: #name) -> Self {
+                value.0
+            }
+        }
+
+        impl std::fmt::Display for #name {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                write!(f, "{}{}", stringify!(#type_name) self.0)
+                write!(f, "{}{}", #type_name, self.0)
             }
         }
     };
