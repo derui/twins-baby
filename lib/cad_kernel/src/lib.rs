a3bb5607dcbe209e414455ab2165f9db09f0ed50
derui <derutakayu@gmail.com>
2026-02-15 18:07:57 +0900

    fix(cad_kernel): define centralized operation kernel

--- lib/cad_kernel/src/lib.rs
+++ lib/cad_kernel/src/lib.rs
@@ -1,2 +1,24 @@
-pub mod pad;
+use cad_base::{
+    feature::{Evaluate, EvaluateError, Feature, FeatureContext, operation::Operation},
+    solid::Solid,
+};
+
+use crate::pad::PadKernel;
+
+mod pad;
 mod sketcher;
+
+/// Kernel for operation. this empty struct only use for static dispatch.
+#[derive(Debug)]
+pub struct OperationKernel;
+
+impl Evaluate for OperationKernel {
+    fn evaluate<'a>(
+        feature: &Feature,
+        context: &FeatureContext<'a>,
+    ) -> color_eyre::eyre::Result<Vec<Solid>, EvaluateError> {
+        match *feature.operation {
+            Operation::Pad(_) => PadKernel::evaluate(feature, context),
+        }
+    }
+}
