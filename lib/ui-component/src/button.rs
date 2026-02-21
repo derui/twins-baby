360beefbf464d29e3611d9a4b13b3651a9bfa565
derui <derutakayu@gmail.com>
2026-02-21 16:47:43 +0900

    feat(ui-component): implement simple button

--- lib/ui-component/src/button.rs
+++ lib/ui-component/src/button.rs
@@ -1,20 +1,34 @@
-use leptos::{
-    component,
-    ev::MouseEvent,
-    html::{self},
-    prelude::*,
-};
+use leptos::{component, ev::MouseEvent, prelude::*};
+use ui_headless::button::use_button;
 
+/// Create button. This button is `icon-based` button, the label is bottom of the button, with fits icon size.
 #[component]
 pub fn Button(
-    #[prop(optional)] _tabindex: Option<i32>,
-    #[prop(optional)] _on_click: Option<Callback<MouseEvent>>,
+    #[prop(optional)] disabled: Option<bool>,
+    #[prop(optional)] tabindex: Option<i32>,
+    #[prop(optional)] on_click: Option<Callback<MouseEvent>>,
+    #[prop(optional)] icon: Option<impl IntoView>,
+    #[prop(optional)] label: Option<ReadSignal<String>>,
 ) -> impl IntoView {
-    let node_ref = NodeRef::<html::Button>::new();
+    let state = use_button(disabled.unwrap_or(false));
 
-    Effect::new(move |_| if let Some(_node) = node_ref.get() {});
-
+    let attrs = (*state.attrs).get();
     view! {
-        <button></button>
+        <button
+            disabled=*attrs.disabled
+            tabindex=tabindex
+            on:click=move |ev| {
+                let Some(handler) = on_click else {
+                    return;
+                };
+                handler.run(ev)
+            }
+            class="inline-flex flex-col items-center w-fit"
+        >
+            {icon}
+            <Show when=move || label.is_some()>
+                <span class="overflow-hidden h-[16px]">{label.unwrap().get()}</span>
+            </Show>
+        </button>
     }
 }
