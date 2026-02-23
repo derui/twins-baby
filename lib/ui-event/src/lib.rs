cc70caa74af4ce388813bcb55f1d3b6bdaef02ef
derui <derutakayu@gmail.com>
2026-02-23 14:07:59 +0900

    feat(ui-event): define some command and notification

--- lib/ui-event/src/lib.rs
+++ lib/ui-event/src/lib.rs
@@ -1,14 +1,91 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+mod types;
+use cad_base::id::{BodyId, PlaneId, SketchId};
+pub use types::*;
+
+use immutable::Im;
+
+use bevy::ecs::message::Message;
+
+/// An event to change the active sketch tool
+///
+/// All event must be read only forcibly.
+#[derive(Message, Debug, Clone)]
+pub struct SketchToolChangeNotification {
+    pub tool: Im<SketchTool>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// Command series for creating sketch. This command must be sequential to send system.
+///
+/// This initiates mode to create sketch, with attracting in bevy's pickup mechanism.
+/// If need to cancel, send `CancelCreateSketchNotification`
+#[derive(Message, Debug, Clone)]
+pub enum CreateSketchCommand {
+    Initiate(Im<String>),
+    PickUpPlane(Im<PlaneId>),
+    Confirm,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/// Cancellation of creating sketch. This will ignore if the system already created, or
+/// already canceled
+#[derive(Message, Debug, Clone)]
+pub struct CancelCreateSketchNotification {}
+
+/// Response of [CreateSketchCommand] . this only return when the [CreateSketchCommand::Confirm] was sent.
+#[derive(Message, Debug, Clone)]
+pub struct CreateSketchCommandReturn {
+    /// sketch id created.
+    pub sketch_id: Im<SketchId>,
+}
+
+/// A command to create body
+#[derive(Message, Debug, Clone)]
+pub struct CreateBodyCommand {
+    pub name: Im<String>,
+}
+
+/// Response of [CreateBodyCommand] .
+#[derive(Message, Debug, Clone)]
+pub struct CreateBodyCommandReturn {
+    pub body_id: Im<BodyId>,
+}
+
+/// An event to notice canvas resize
+#[derive(Message, Debug, Clone)]
+pub struct CanvasResizeNotification {
+    pub width: Im<u32>,
+    pub height: Im<u32>,
+}
+
+/// An notification to notice mouse movement.
+///
+/// This is only for bevy, nad client x/y is client position of the canvas
+#[derive(Message, Debug, Clone)]
+pub struct MouseMovementNotification {
+    pub delta_x: Im<i32>,
+    pub delta_y: Im<i32>,
+    /// Last point of moved in canvas
+    pub client_x: Im<u32>,
+    pub client_y: Im<u32>,
+}
+
+/// DOM's mousedown event representation
+#[derive(Message, Debug, Clone)]
+pub struct MouseDownNotification {
+    /// Last point of moved in canvas
+    pub client_x: Im<u32>,
+    pub client_y: Im<u32>,
+
+    /// pressed button on event.
+    pub button: Im<MouseButton>,
+}
+
+/// DOM's mousedown event representation
+#[derive(Message, Debug, Clone)]
+pub struct MouseUpNotification {
+    /// Last point of moved in canvas
+    pub client_x: Im<u32>,
+    pub client_y: Im<u32>,
+
+    /// pressed button on event.
+    pub button: Im<MouseButton>,
 }
---
+++ lib/ui-event/src/types.rs
@@ -0,0 +1,56 @@
+use color_eyre::eyre;
+
+/// Tool selection for sketch mode
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum SketchTool {
+    Line,
+    Circle,
+    Rectangle,
+}
+
+/// Kind of perspective
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default, Hash)]
+pub enum PerspectiveKind {
+    #[default]
+    Feature,
+    Sketch,
+}
+
+impl PerspectiveKind {
+    /// Create a `PerspectiveKind` from a string
+    #[tracing::instrument]
+    pub fn from_string(str: &str) -> eyre::Result<PerspectiveKind> {
+        match str {
+            "Feature" => Ok(PerspectiveKind::Feature),
+            "Sketch" => Ok(PerspectiveKind::Sketch),
+            _ => Err(eyre::eyre!("Invalid perspective kind: {}", str)),
+        }
+    }
+}
+
+impl ToString for PerspectiveKind {
+    fn to_string(&self) -> String {
+        match self {
+            PerspectiveKind::Feature => "Feature".to_string(),
+            PerspectiveKind::Sketch => "Sketch".to_string(),
+        }
+    }
+}
+
+impl std::str::FromStr for PerspectiveKind {
+    type Err = eyre::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Self::from_string(s)
+    }
+}
+
+/// Mouse button representation.
+///
+/// NOTE: we have bevy's mouse input, but it is for bevy, this is for our system
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum MouseButton {
+    Left,
+    Right,
+    Center,
+}
