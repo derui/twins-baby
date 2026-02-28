e62124417e49083e2cf6b87b0ac242611864031c
derui <derutakayu@gmail.com>
2026-02-28 14:30:38 +0900

    feat(ui-event-macros): implement notification derive macro

---
+++ lib/ui-event-macros/src/notification.rs
@@ -0,0 +1,32 @@
+use proc_macro::TokenStream;
+use quote::quote;
+
+/// implement of Notification derive macro
+pub(crate) fn impl_notification_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+
+    if !name.to_string().ends_with("Notification") {
+        panic!("[Notification] macro needs the type name ends with `Notification`");
+    }
+
+    let generated = quote! {
+        impl crate::command::Notification for #name {
+            fn correlation_id(&self) -> &Command {
+                &(*self.correlation_id)
+            }
+
+            fn select_ref<T: Notification + 'static>(&self) -> Option<&T> {
+                let type_id = std::any::TypeId::of::<T>();
+                let current = std::any::TypeId::of::<#name>();
+
+                if type_id == current {
+                    let current: Box<&dyn std::any::Any> = Box::new(self);
+                    current.downcast_ref::<T>()
+                } else {
+                    None
+                }
+            }
+        }
+    };
+    generated.into()
+}
