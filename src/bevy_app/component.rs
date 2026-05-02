a305d5212e9c95b4c004146a132b2b1464803f8c
derui <derutakayu@gmail.com>
2026-05-02 13:55:51 +0900

    style: apply lint

--- src/bevy_app/component.rs
+++ src/bevy_app/component.rs
@@ -1,10 +1,6 @@
 // List of components
 
 use bevy::ecs::component::Component;
-use cad_base::{
-    body::PlaneRef,
-    id::{EdgeId, FaceId},
-};
 use ui_event::ObjectType;
 
 /// Enum of types of object in CAD. This uses to pick, edit, and move.
