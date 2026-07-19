8ecfa8e9451664c9f3af07d05b560f8af344a015
derui <derutakayu@gmail.com>
2026-07-19 10:38:34 +0900

    refactor: Update point ID variable tracking in geometry

--- lib/cad-base/src/sketch/geometry.rs
+++ lib/cad-base/src/sketch/geometry.rs
@@ -1,21 +1,21 @@
 use immutable::Im;
 
-use crate::{
-    id::VariableId,
-    sketch::{Point2, scope::VariableScope},
+use crate::sketch::{
+    Point2,
+    scope::{VariableArena, VariableIndex},
 };
 
 /// A basic structure of the sketch. This is representation of a line and points.
 #[derive(Debug, Clone)]
 pub struct LineSegment {
     /// ID mapping for points.
-    pub start_points: Im<(VariableId, VariableId)>,
-    pub end_points: Im<(VariableId, VariableId)>,
+    pub start_points: Im<(VariableIndex, VariableIndex)>,
+    pub end_points: Im<(VariableIndex, VariableIndex)>,
 }
 
 impl LineSegment {
     /// Make a new line with points.
-    pub fn from_points(start: &Point2, end: &Point2, registrar: &mut VariableScope) -> Self {
+    pub fn from_points(start: &Point2, end: &Point2, registrar: &mut VariableArena) -> Self {
         let start_ids = (registrar.register(*start.x), registrar.register(*start.y));
         let end_ids = (registrar.register(*end.x), registrar.register(*end.y));
 
