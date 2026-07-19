87aab9df2cf3c08845a675af735e0e78b72743ff
derui <derutakayu@gmail.com>
2026-07-19 10:16:26 +0900

    feat(cad-base): add some tags

--- lib/cad-base/src/tag.rs
+++ lib/cad-base/src/tag.rs
@@ -3,3 +3,11 @@
 /// A tag is a unique identifier for an entity in the CAD model. It is used to reference entities in the model and to store metadata about them.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct FaceTag(u64);
+
+/// A tag is a unique identifier for a variable in the CAD model.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct VariableTag(u64);
+
+/// A tag is a unique identifier for a solid in the CAD model.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct SolidTag(u64);
